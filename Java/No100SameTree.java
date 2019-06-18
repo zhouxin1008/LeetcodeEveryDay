09305feb78d98b671d34b106ac09357ccff5367f
--local <zhou_xin@worksap.co.jp>
2019-06-18 10:22:12 +0800

    No100 Same Tree

---
+++ Java/No100SameTree.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=100 lang=java
+ *
+ * [100] Same Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class No100SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null) return false;
+        if(q==null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
