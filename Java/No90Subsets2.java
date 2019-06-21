48f28d2741aea21f4b4100e1baac79b3e8d490e1
--local <zhou_xin@worksap.co.jp>
2019-06-21 14:13:41 +0800

    No90 Subsets II

---
+++ Java/No90Subsets2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=90 lang=java
+ *
+ * [90] Subsets II
+ */
+class No90Subsets2 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result= new ArrayList<>();
+        dfs(nums,0,new ArrayList<Integer>(),result);
+        return result;
+    }
+    
+    public void dfs(int[] nums,int index,List<Integer> path,List<List<Integer>> result){
+        result.add(path);
+        for(int i=index;i<nums.length;i++){
+            if(i>index&&nums[i]==nums[i-1]) continue;
+            List<Integer> nPath= new ArrayList<>(path);
+            nPath.add(nums[i]);
+            dfs(nums,i+1,nPath,result);
+        }
+    }
+}
+
