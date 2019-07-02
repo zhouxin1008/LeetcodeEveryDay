0bfd0d62fe22ab7d46b9ef2b07fc5f51fe0f0f35
zhouxin19911008 <zhouxin19911008@gmail.com>
2019-07-02 17:45:55 +0800

    No145 Binary Tree Postorder Traversal

---
+++ Java/No145BinaryTreePostorderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class No145BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while(!stack.isEmpty() || p != null) {
+            if(p != null) {
+                stack.push(p);
+                result.addFirst(p.val);  // Reverse the process of preorder
+                p = p.right;             // Reverse the process of preorder
+            } else {
+                TreeNode node = stack.pop();
+                p = node.left;           // Reverse the process of preorder
+            }
+        }
+        return result;
+    }
+}
+
