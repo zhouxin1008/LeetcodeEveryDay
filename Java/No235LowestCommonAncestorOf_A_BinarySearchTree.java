11f0f36fa7f26fc331812e464efc1374e966fd8e
zhouxin19911008 <zhouxin19911008@gmail.com>
2019-07-17 16:23:10 +0800

    No235 Lowest Common Ancestor Of A Binary Search Tree

---
+++ Java/No235LowestCommonAncestorOf_A_BinarySearchTree.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=235 lang=java
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class No235LowestCommonAncestorOf_A_BinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p ,q);
+        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p ,q);
+        return root;
+    }
+}
+
