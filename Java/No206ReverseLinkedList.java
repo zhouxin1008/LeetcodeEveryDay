91194caa7ac9a68c173cbff8ff0312d6b55e8301
zhouxin19911008 <zhouxin19911008@gmail.com>
2019-07-18 10:17:29 +0800

    No206 Reverse Linked List

---
+++ Java/No206ReverseLinkedList.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class No206ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode pre = head;
+        ListNode cur = head.next;
+        pre.next = null;
+        while(cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
+
