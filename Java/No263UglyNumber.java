77baa56fada586287856e182fdb4d21d34ed7485
zhouxin19911008 <zhouxin19911008@gmail.com>
2019-08-02 14:23:36 +0800

    No263 Ugly Number

---
+++ Java/No263UglyNumber.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=263 lang=java
+ *
+ * [263] Ugly Number
+ */
+class No263UglyNumber {
+    public boolean isUgly(int num) {
+        if(num <= 0) return false;
+        while(num % 2 == 0){
+            num = num / 2;
+        }
+        while(num % 3 == 0){
+            num = num / 3;
+        }
+        while(num % 5 == 0){
+            num = num / 5;
+        }
+        return num == 1;
+    }
+}
+
